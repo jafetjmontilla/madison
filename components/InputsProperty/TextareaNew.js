18de044e92e8187812478b4f3ba1728b05c9aa19
Jafet <febmerlib@gmail.com>
2024-02-14 00:25:23 +0000

    test2

--- components/InputsProperty/TextareaNew.js
+++ components/InputsProperty/TextareaNew.js
@@ -5,6 +5,12 @@ export const TextareaNew = (props) => {
   const [field, meta, helpers] = useField(props);
 
   return (
-    <Textarea value={field?.value} meta={meta} setValue={helpers?.setValue} />
+    <>
+      <div>
+        <label className="capitalize text-xs">{props?.label}</label>
+        {meta.error && <span className="text-red-500 text-xs ml-2">!requerido</span>}
+      </div>
+      <Textarea value={field?.value} meta={meta} setValue={helpers?.setValue} />
+    </>
   )
 }
